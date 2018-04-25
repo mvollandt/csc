31d57f2e6041087ed1a32f69aef7bc9dbbf3a844
mvollandt <38690461+mvollandt@users.noreply.github.com>
2018-04-25 12:40:07 +0200

    Initial version

---
+++ csc_checks.py
@@ -0,0 +1,35 @@
+# name: csc_checks.py
+# desc: check definitions
+
+
+csc1_1 = {'check_type': 'simple_in',
+          'match': 'banner motd',
+          'result_ok': 'This test has been successful.',
+          'result_missing': 'Test failed.',
+          'info': 'Some additional information.',
+          'fix': 'Command to fix'}
+csc1_2 = {'check_type': 'simple_in',
+          'match': 'no cdp enable',
+          'result_ok': 'This test has been successful.',
+          'result_missing': 'Test failed.',
+          'info': 'Some additional information.',
+          'fix': 'Command to fix'}
+csc1_3 = {'check_type': 'simple_in',
+          'match': 'xxxx',
+          'result_ok': 'This test has been successful.',
+          'result_missing': 'Test failed.',
+          'info': 'Some additional information.',
+          'fix': 'Command to fix'}
+csc1_4 = {'check_type': 'simple_in',
+          'match': 'xxxx',
+          'result_ok': 'This test has been successful.',
+          'result_missing': 'Test failed.',
+          'info': 'Some additional information.',
+          'fix': 'Command to fix'}
+csc1_5 = {'check_type': 'simple_in',
+          'match': 'xxxx',
+          'result_ok': 'This test has been successful.',
+          'result_missing': 'Test failed.',
+          'info': 'Some additional information.',
+          'fix': 'Command to fix'}
+
