1c681c97cdd14bf90a4eb08ddfdfc8805783bfd0
mvollandt <38690461+mvollandt@users.noreply.github.com>
2018-04-25 10:53:18 +0200

    Intial version (with examples)

---
+++ csc_devices.py
@@ -0,0 +1,21 @@
+# name: csc_devices.py
+# desc: lists all devices with devicename, IP, username, password, secret
+
+router_013 = {'device_name': 'router_013', 'device_type': 'cisco_nxos', 'ip': '1.1.1.10', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+router_023 = {'device_name': 'router_023', 'device_type': 'cisco_nxos', 'ip': '1.1.1.11', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+router_014 = {'device_name': 'router_014', 'device_type': 'cisco_nxos', 'ip': '1.1.1.26', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+router_024 = {'device_name': 'router_024', 'device_type': 'cisco_nxos', 'ip': '1.1.1.27', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+
+switch_013 = {'device_name': 'switch_013', 'device_type': 'cisco_nxos', 'ip': '1.1.1.3', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_023 = {'device_name': 'switch_023', 'device_type': 'cisco_nxos', 'ip': '1.1.1.4', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_033 = {'device_name': 'switch_033', 'device_type': 'cisco_nxos', 'ip': '1.1.1.5', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_043 = {'device_name': 'switch_043', 'device_type': 'cisco_nxos', 'ip': '1.1.1.6', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_053 = {'device_name': 'switch_053', 'device_type': 'cisco_nxos', 'ip': '1.1.1.7', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_063 = {'device_name': 'switch_063', 'device_type': 'cisco_nxos', 'ip': '1.1.1.8', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_014 = {'device_name': 'switch_014', 'device_type': 'cisco_nxos', 'ip': '1.1.1.19', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_024 = {'device_name': 'switch_024', 'device_type': 'cisco_nxos', 'ip': '1.1.1.20', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_034 = {'device_name': 'switch_034', 'device_type': 'cisco_nxos', 'ip': '1.1.1.21', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_044 = {'device_name': 'switch_044', 'device_type': 'cisco_nxos', 'ip': '1.1.1.22', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_054 = {'device_name': 'switch_054', 'device_type': 'cisco_nxos', 'ip': '1.1.1.23', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+switch_064 = {'device_name': 'switch_064', 'device_type': 'cisco_nxos', 'ip': '1.1.1.24', 'username': 'dummy_username', 'password': 'dummy_password', 'port': 22, 'verbose': False, 'secret': 'dummy_secret', }
+
