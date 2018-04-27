9324dd5ac1d9c5a4787853567c06bfb1acb0f983
Malte Vollandt <github863027@s245050704.online.de>
2018-04-27 16:05:59 +0200

    refactor and add details for simple tests

--- csc_checks.py
+++ csc_checks.py
@@ -2,34 +2,36 @@
 # desc: check definitions
 
 
-csc1_1 = {'check_type': 'simple_in',
+csc1_1 = {'check_type': 'check_in_simple',
           'match': 'banner motd',
           'result_ok': 'This test has been successful.',
-          'result_missing': 'Test failed.',
+          'result_failed': 'Test failed.',
           'info': 'Some additional information.',
           'fix': 'Command to fix'}
-csc1_2 = {'check_type': 'simple_in',
+csc1_2 = {'check_type': 'check_in_simple',
           'match': 'no cdp enable',
           'result_ok': 'This test has been successful.',
-          'result_missing': 'Test failed.',
+          'result_failed': 'Test failed.',
           'info': 'Some additional information.',
           'fix': 'Command to fix'}
-csc1_3 = {'check_type': 'simple_in',
+csc1_3 = {'check_type': 'check_not_in_simple',
+          'match': 'snmpv1',
+          'result_ok': 'This test has been successful.',
+          'result_failed': 'Test failed.',
+          'info': 'Some additional information.',
+          'fix': 'Command to fix'}
+csc1_4 = {'check_type': 'check_not_in_simple',
           'match': 'xxxx',
           'result_ok': 'This test has been successful.',
-          'result_missing': 'Test failed.',
+          'result_failed': 'Test failed.',
           'info': 'Some additional information.',
           'fix': 'Command to fix'}
-csc1_4 = {'check_type': 'simple_in',
+
+csc1_5 = {'check_type': 'check_parameter',
           'match': 'xxxx',
           'result_ok': 'This test has been successful.',
-          'result_missing': 'Test failed.',
+          'result_failed': 'Test failed.',
           'info': 'Some additional information.',
           'fix': 'Command to fix'}
-csc1_5 = {'check_type': 'simple_in',
-          'match': 'xxxx',
-          'result_ok': 'This test has been successful.',
-          'result_missing': 'Test failed.',
-          'info': 'Some additional information.',
-          'fix': 'Command to fix'}
+
 
